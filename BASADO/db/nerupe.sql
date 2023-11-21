f50989bacfdacc93d6feac657a8d1dbe3175751e
Aikarubi <nerupe@floridauniversitaria.es>
2023-11-21 12:51:04 +0100

    Todos los archivos

---
+++ BASADO/db/nerupe.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `persona`
+(
+  `id` int
+(11) NOT NULL,
+  `nombre` varchar
+(20) NOT NULL,
+  `estilo` varchar
+(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `persona` (`
+id`,
+`nombre
+`,`estilo`) VALUES
+(1, 'Nerea', 'Phonk'),
+(2, 'Fer', 'Pop');
